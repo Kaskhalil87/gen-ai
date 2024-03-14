6687b09f538467d4d8463f2bfb8987581d46942b
Kaskhalil87 <140751168+Kaskhalil87@users.noreply.github.com>
2024-03-14 14:13:58 -0500

    implemented params and completed ui

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -8,7 +8,7 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
 
       <MobileNav />
       <div className="root-container">
-        {/* <div className="wrapper">{children}</div> */}
+        <div className="wrapper">{children}</div>
       </div>
     </main>
   );
